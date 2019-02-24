2b4aaef4128371cd1c754270c1ee3373584f983c
Yidir <yader.dev@gmail.com>
2019-02-24 02:58:16 +0100

    edit title again
    Signed-off-by: Yidir <yader.dev@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel New title
+                    Laravel New title 2
                 </div>
 
                 <div class="links">
