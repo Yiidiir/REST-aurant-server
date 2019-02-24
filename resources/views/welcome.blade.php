cf008f077264446a0b25ade3da49cc6dcd3debdd
Yidir <yader.dev@gmail.com>
2019-02-24 01:34:49 +0100

    Added XDebug config
    Signed-off-by: Yidir <yader.dev@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel New title
                 </div>
 
                 <div class="links">
