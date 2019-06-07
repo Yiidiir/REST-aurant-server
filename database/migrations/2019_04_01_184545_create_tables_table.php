fbd54e13da03794ab8167ee5b9bb53f961fc2355
Yidir <yader.dev@gmail.com>
2019-06-07 03:45:50 +0100

    Added s to auto generated migrations
    Signed-off-by: Yidir <yader.dev@gmail.com>

--- database/migrations/2019_04_01_184545_create_tables_table.php
+++ database/migrations/2019_04_01_184545_create_tables_table.php
@@ -15,9 +15,13 @@ class CreateTablesTable extends Migration
     {
         Schema::create('tables', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('capacite_min');
-            $table->integer('capacite_max');
-            $table->tinyInteger('classe');
+            $table->integer('capacity_min');
+            $table->integer('capacity_max');
+            $table->tinyInteger('class');
+            $table->integer('in_restaurant_number');
+            $table->boolean('available');
+            $table->unsignedInteger('restaurant_id');
+            $table->foreign('restaurant_id')->references('id')->on('restaurants');
         });
     }
 
