e4766e14c0c38d9c2b8331cee96d295f0a1cea36
Yidir <yader.dev@gmail.com>
2019-06-09 22:16:08 +0100

    --ammend
    Signed-off-by: Yidir <yader.dev@gmail.com>

--- database/factories/RestaurantFactory.php
+++ database/factories/RestaurantFactory.php
@@ -5,6 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Restaurant::class, function (Faker $faker) {
     return [
         'id' => $faker->unique()->randomNumber,
+        'name' => $faker->company,
         'address' => $faker->address,
         'class' => $faker->numberBetween(1, 5),
         'created_at' => $faker->dateTimeThisYear($max = 'now', $timezone = null),
--- database/migrations/2019_04_01_184516_create_restaurants_table.php
+++ database/migrations/2019_04_01_184516_create_restaurants_table.php
@@ -15,6 +15,7 @@ class CreateRestaurantsTable extends Migration
     {
         Schema::create('restaurants', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
             $table->string('address');
             $table->tinyInteger('class');
             $table->timestamps();
