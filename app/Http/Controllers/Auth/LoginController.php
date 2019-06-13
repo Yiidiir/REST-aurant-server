989601d826a3a12fdf56ed30d58b0c2cc0fe0285
Yidir <yader.dev@gmail.com>
2019-06-13 12:00:32 +0100

    Made jsonResource for user
    Signed-off-by: Yidir <yader.dev@gmail.com>

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\User as UserResource;
 
 
 class LoginController extends Controller
@@ -47,9 +48,7 @@ class LoginController extends Controller
             $user = $this->guard()->user();
             $user->generateToken();
 
-            return response()->json([
-                'data' => $user->toArray(),
-            ]);
+            return new UserResource($user);
         }
 
         return $this->sendFailedLoginResponse($request);
---
+++ app/Http/Resources/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+
+/**
+ * Class User
+ * @package App\Http\Resources
+ */
+class User extends JsonResource
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->resource->id,
+            'first_name' => $this->resource->first_name,
+            'last_name' => $this->resource->last_name,
+            'email' => $this->resource->email,
+            'role' => $this->resource->role,
+            'api_token' => $this->resource->api_token,
+        ];
+    }
+}
