cc6faa8150c80f0ed4bc730a2decf17e29a95df0
Yidir <yader.dev@gmail.com>
2019-06-30 09:57:30 +0100

    Added phone number to user resource
    Signed-off-by: Yidir <yader.dev@gmail.com>

--- app/Http/Resources/User.php
+++ app/Http/Resources/User.php
@@ -24,6 +24,7 @@ class User extends JsonResource
             'first_name' => $this->resource->first_name,
             'last_name' => $this->resource->last_name,
             'email' => $this->resource->email,
+            'phone_number' => $this->resource->phone_number,
             'role' => $this->resource->role,
             'api_token' => $this->resource->api_token,
             'join_date' => $this->created_at->format('l, d F Y'),
